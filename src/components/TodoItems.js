aa17c0ca12f6a83bad5899106b1b10596cb050d0
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-04-06 23:15:18 -0700

    yes
    yes

---
+++ src/components/TodoItems.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Hello Hello hihihihihi
+                </p>
+            </div>
+        )
+    }
+}
+
+export default TodoItem
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItems';
+
+class Todos extends Component{
+    render() {
+        return this.props.todos.map((todo) => (
+            <TodoItem />
+        ));
+    }
+}
+
+export default Todos;
